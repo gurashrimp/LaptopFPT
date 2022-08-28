5144b30f47049b71a79dc2dfe7f31367510570c1
datct1602 <104557228+datct1602@users.noreply.github.com>
2022-08-28 13:51:00 +0700

    Folder Admin Web
    Mấy ông code bên web download folder này về code chung trong đây nha

---
+++ LaptopShop/routes/router.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET EMPLOYEE. */
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+/* GET PRODUCT. */
+module.exports = router;
